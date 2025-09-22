aec33fa3a7b4695a3c74a92888033c2d299a1957
Arriana3 <amm13857@email.vccs.edu>
2025-09-21 22:34:43 -0400

    Add project files.

---
+++ Chapter 3 Exercise 17.cpp
@@ -0,0 +1,23 @@
+/*File name:Chapter 3 Exercise 17.cpp
+  Programmer:Arriana Maldonado
+  Date:9/21/2025
+  Purpose: Write a programe that produces two random nukmbers and then pause while the
+  user works through the question of adding the two numbers. When the user presses enter, the answer
+  is displayed.
+*/ 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{srand(time(0));
+
+    int num1, num2, sum;
+    num1 = rand() % 1000;
+    num2 = rand() % 1000;
+    cout << setw(6) << num1 << endl;
+    cout << " + " << setw(3) << num2 << endl;
+    cin.ignore();
+    cin.get();
+    sum = num1 + num2;
+    cout << "The answer is " << sum << ".\n";
+}
